a6f24506580a125413306c4a9ea77c129e4e8803
Juliana-flores <floresjuliana@live.com>
2025-09-28 12:23:39 -0300

    Initial setup with project files

---
+++ src/config/ormConfig.ts
@@ -0,0 +1,14 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+const options: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.TYPEORM_HOST,
+  port: +process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  entities: ['src/modules/**/entities/**.ts'],
+  migrations: ['src/database/migrations/*{.js,.ts}'],
+};
+
+export const connectionSource = new DataSource(options);
