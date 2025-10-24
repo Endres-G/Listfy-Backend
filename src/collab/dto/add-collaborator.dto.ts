8e6390bb964a28c662a384385d938366ad5cf52b
Sergio Sequeira Laurino <122753482+sergioslaurino@users.noreply.github.com>
2025-10-24 16:31:57 -0300

    Create add-collaborator.dto.ts

---
+++ src/collab/dto/add-collaborator.dto.ts
@@ -0,0 +1,15 @@
+import { IsEmail, IsIn, IsOptional, IsString } from 'class-validator';
+import { CollaboratorRole } from '../entities/list-collaborator.entity';
+
+export class AddCollaboratorDto {
+  @IsOptional()
+  @IsString()
+  userId?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsIn(['viewer', 'editor'])
+  role: CollaboratorRole; // 'owner' não é exposto via API pública
+}
