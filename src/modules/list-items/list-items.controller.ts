a27a835c021729dc5631ff9277fc7a1b5dc1b60c
Vitor Freitas <vitordeavilafreitas@gmail.com>
2025-11-10 00:05:27 -0300

    feat: creating edit and delete endpoints for list-items

--- src/modules/list-items/list-items.controller.ts
+++ src/modules/list-items/list-items.controller.ts
@@ -1,15 +1,11 @@
-import {
-  Body,
-  Controller,
-  Param,
-  Post,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Param, Post, Put } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { GetUser } from 'src/decorators/get-user.decorator';
 import { User } from '../users/entities/user.entity';
 import { ListItemsService } from './list-items.service';
 import { CreateListItemDto } from './dto/create-list-item.dto';
 import { AssignListItemDto } from './dto/assign-list-item.dto';
+import { EditListItemDto } from './dto/edit-list-item.dto';
 
 @ApiTags('List Items')
 @Controller('lists/:listId/items')
@@ -33,5 +29,23 @@ export class ListItemsController {
     @GetUser() user: User,
   ) {
     return this.listItemsService.assign(listId, itemId, dto, user);
+  }
+  @Put(':itemId')
+  edit(
+    @Param('listId') listId: number,
+    @Param('itemId') itemId: number,
+    @Body() dto: EditListItemDto,
+    @GetUser() user: User,
+  ) {
+    return this.listItemsService.edit(listId, itemId, dto, user);
+  }
+
+  @Delete(':itemId')
+  delete(
+    @Param('listId') listId: number,
+    @Param('itemId') itemId: number,
+    @GetUser() user: User,
+  ) {
+    return this.listItemsService.delete(listId, itemId, user);
   }
 }
